6c2e916156a471d375da362e5f2f9167fa542bce
yauseyea <yauseyea@MSGN03253.int.root.msg.ag>
2016-10-31 20:10:41 +0100

    Test Class for Horse

---
+++ VadereState/tests/org/vadere/state/scenario/TestHorse.java
@@ -0,0 +1,49 @@
+package org.vadere.state.scenario;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.vadere.state.attributes.scenario.AttributesHorse;
+
+public class TestHorse {
+
+	private Horse horse;
+	
+	@Before
+	public void SetUp() {
+		this.horse = createHorse();
+		horse.setNextTargetListIndex(0);
+	}
+	
+	@Test
+	public void testSetNextTargetListIndex() {
+		horse.setNextTargetListIndex(1);
+		assertEquals(1, horse.getNextTargetListIndex());
+	}
+	
+	@Test
+	public void testIncrementNextTargetListIndex() {
+		assertEquals(0, horse.getNextTargetListIndex());
+		horse.incrementNextTargetListIndex();
+		assertEquals(1, horse.getNextTargetListIndex());
+	}
+
+	@Test
+	public void testGetNextTargetId() {
+		horse.getTargets().add(3);
+		assertEquals(3, horse.getNextTargetId());
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetNextTargetIdFail() {
+		horse.getTargets().clear();
+		horse.getNextTargetId();
+	}
+	
+	private Horse createHorse() {
+		return new Horse(new AttributesHorse(), new Random(0));
+	}
+}

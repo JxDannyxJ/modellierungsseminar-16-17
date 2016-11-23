93ddd10f2f12b3719c69641688c90f6c076d7c47
Huber Hager <hager2@hm.edu>
2016-11-23 15:13:25 +0100

    parallel update scheme available for agentosm

--- VadereSimulator/src/org/vadere/simulator/models/osm/updateScheme/ParallelWorkerOSM.java
+++ VadereSimulator/src/org/vadere/simulator/models/osm/updateScheme/ParallelWorkerOSM.java
@@ -1,24 +1,24 @@
 package org.vadere.simulator.models.osm.updateScheme;
 
-import org.vadere.simulator.models.osm.PedestrianOSM;
+import org.vadere.simulator.models.osm.AgentOSM;
 import org.vadere.simulator.models.osm.updateScheme.UpdateSchemeOSM.CallMethod;
 
 public class ParallelWorkerOSM implements Runnable {
 
-	private final PedestrianOSM ped;
+	private final AgentOSM agent;
 	private final CallMethod callMethod;
 	private final double timeStepInSec;
 
-	public ParallelWorkerOSM(CallMethod callMethod, PedestrianOSM ped,
+	public ParallelWorkerOSM(CallMethod callMethod, AgentOSM agent,
 			double timeStepInSec) {
-		this.ped = ped;
+		this.agent  = agent;
 		this.callMethod = callMethod;
 		this.timeStepInSec = timeStepInSec;
 	}
 
 	@Override
 	public void run() {
-		ped.update(timeStepInSec, -1, callMethod);
+		agent.update(timeStepInSec, -1, callMethod);
 	}
 
 }
